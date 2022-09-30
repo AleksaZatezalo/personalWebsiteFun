09db3ed72d10f08f36baac47984770882c0df759
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-09-30 19:28:27 -0400

    Components Imported Successfully.

--- react-portfolio-website-1/src/App.jsx
+++ react-portfolio-website-1/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import Header from "./components/header/Header"
+import Nav from "./components/nav/Nav"
+import About from "./components/about/About"
+import Experiance from "./components/experiance/Experiance"
+import Services from "./components/servises/Services"
+import Portfolio from "./components/portfolio/Portfolio"
+import Testimonials from "./components/testimonials/Testimonials"
+import Contact from "./components/contact/Contact"
+import Footer from "./components/footer/Footer"
 
 const App = () => {
     return (
         <>
             <Header />
-
+            <Nav />
+            <About />
+            <Experiance />
+            <Services />
+            <Portfolio />
+            <Testimonials /> 
+            <Contact />
+            <Footer />
         </>
     )
 }
