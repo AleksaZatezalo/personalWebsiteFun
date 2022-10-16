5a7beeb76dbdfd25049f22190bf1f0ee0711c660
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-16 19:35:21 -0400

    Contact styling finishesd. Now it's time to add Email capability.

--- react-portfolio-website-1/src/components/contact/Contact.jsx
+++ react-portfolio-website-1/src/components/contact/Contact.jsx
@@ -11,19 +11,21 @@ const Contact = () => {
             <div className='container contact__container'>
                 <div className="contact__options">
                     <article className='contact__option'>
-                        <MdOutlineEmail/>
+                        <MdOutlineEmail className='contact__option-icon'/>
                         <h4>Email</h4>
                         <h5>zabumaphu@gmail.com</h5>
                         <a href="mailto:zabumaphu@gmail.com">Send a Message</a>
                     </article>
                 </div>
+                <form action="">
+                    <input type="text" name='name' placeholder="Your Full Name" required/>
+                    <input type="email" name='email' placeholder="Your Email" required/>
+                    <textarea name="messahe" rows="7" placeholder="Your Message" required></textarea>
+                    <button type="submit" className='btn btn-primary'>Send Message</button>
+                </form>
+                
             </div>
-            <form action="">
-                <input type="text" name='name' placeholder="Your Full Name" required/>
-                <input type="email" name='email' placeholder="Your Email" required/>
-                <textarea name="messahe" rows="7" placeholder="Your Message" required></textarea>
-                <button type="submit" className='btn btn-primary'>Send Message</button>
-            </form>
+            
         </section>
     )
 }
