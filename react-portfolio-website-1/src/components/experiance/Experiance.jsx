edade2616577a0bfa8040b96a6328bb4e4652471
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-16 16:28:35 -0400

    Experiance Styling Complete.

--- react-portfolio-website-1/src/components/experiance/Experiance.jsx
+++ react-portfolio-website-1/src/components/experiance/Experiance.jsx
@@ -10,68 +10,87 @@ const Experiance = () => {
             <h2>My Experiance</h2>
             <div className='container experiance__container'>
                 <div className='experiance__cloudSec'>
-                    <h3>Cloud Security</h3>
+                    <h3> Cloud Security</h3>
                     <div className="experiance__content">
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Azure Active Directory</h4>
-                            <small className='text-light'>Experainced</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Azure Active Directory</h4>
+                                <small className='text-light'>Experainced</small>
+                            </div>
+                        
                         </article>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Security Roles in Power Platform</h4>
-                            <small className='text-light'>Experainced</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>DevSecOps</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                            
                         </article>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>DevSecOps</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Security Strategy</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                            
                         </article>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Cyber Security Strategy</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Zero Trust</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                           
                         </article>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Zero Trust</h4>
-                            <small className='text-light'>Intermediate</small>
-                        </article>
-                        <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>PenetrationTesting</h4>
-                            <small className='text-light'>Intermediate</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Penetration Testing</h4>
+                                <small className='text-light'>Intermediate</small>
+                            </div>
+                            
                         </article>
                     </div>
 
                 </div>
                 <div className='experiance__backend'>
+                <h3> Backend Development</h3>
+
                 <div className="experiance__content">
-                        <h3>Backend Development</h3>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Microsoft Azure</h4>
-                            <small className='text-light'>Experainced</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Microsoft Azure</h4>
+                                <small className='text-light'>Experainced</small>
+                            </div>
+                            
+                        </article>
+
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Android Application Dev</h4>
+                                <small className='text-light'>Experainced</small>
+                            </div>
+                            
+                        </article>
+                       
+                        <article className='experiance__details'>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Full Stack Web Dev</h4>
+                                <small className='text-light'>Experainced</small>
+                            </div>
+                            
                         </article>
                         <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Databases</h4>
-                            <small className='text-light'>Experainced</small>
-                        </article>
-                        <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Full Stack Web Development</h4>
-                            <small className='text-light'>Experainced</small>
-                        </article>
-                        <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Android Application Development</h4>
-                            <small className='text-light'>Intermediate</small>
-                        </article>
-                        <article className='experiance__details'>
-                            <BsPatchCheckFill/>
-                            <h4>Artificial Intelligence</h4>
-                            <small className='text-light'>Begginer</small>
+                            <BsPatchCheckFill className='experiance__details-icon '/>
+                            <div>
+                                <h4>Artificial Intelligence</h4>
+                                <small className='text-light'>Begginer</small>
+                            </div>
                         </article>
                     </div>
                     
