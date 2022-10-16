b8b53397f8a51ec6e1bfdddad179f71ef2e11dad
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-16 18:05:08 -0400

    Portfolio Style finally done -- FINALLY

--- react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website-1/src/components/portfolio/Portfolio.jsx
@@ -9,22 +9,23 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className='container portfolio__container'>
-        <article className='portolio_item'>
-          <dev className="portolio_item_image">
+        <article className='portfolio__item'>
+          <dev className="portfolio__item-image">
             <img src={CHECKLIST} alt=""/>
           </dev>
           <h3>Checklist Application</h3>
-          <a href="https://github.com/AleksaZatezalo/ChecklistApp" classname='btn' targer='_blank'>Github</a>
+          <a href="https://github.com/AleksaZatezalo/ChecklistApp" className='btn' target='_blank'>Github</a>
           <a href="https://play.google.com/store/apps/details?id=com.aleksa.checklist" className='btn btn-primary' targer='_blank'>Live Demo</a>
         </article>
-        <article className='portolio_item'>
-          <dev className="portolio_item_image">
+        <article className='portfolio__item'>
+          <dev className="portfolio__item-image">
             <img src={QUIZ} alt=""/>
           </dev>
           <h3>Quiz Application</h3>
-          <a href="https://github.com/AleksaZatezalo/quizApp" classname='btn' targer='_blank'>Github</a>
+          <a href="https://github.com/AleksaZatezalo/quizApp" className='btn' target='_blank'>Github</a>
           <a href="https://play.google.com/store/apps/details?id=com.aleksa.quizapp" className='btn btn-primary' targer='_blank'>Live Demo</a>
         </article>
+        
       </div>
     </section>
   )
