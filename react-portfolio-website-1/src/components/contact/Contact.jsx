94c3a1319899177525317dd01f7cb076f4c7d607
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-16 19:06:27 -0400

    Contact component finished.

--- react-portfolio-website-1/src/components/contact/Contact.jsx
+++ react-portfolio-website-1/src/components/contact/Contact.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import "./contact.css"
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
-            <h2>Contact Me</h2>
+            <h2>Contact</h2>
+
+            <div className='container contact__container'>
+                <div className="contact__options">
+                    <article className='contact__option'>
+                        <MdOutlineEmail/>
+                        <h4>Email</h4>
+                        <h5>zabumaphu@gmail.com</h5>
+                        <a href="mailto:zabumaphu@gmail.com">Send a Message</a>
+                    </article>
+                </div>
+            </div>
+            <form action="">
+                <input type="text" name='name' placeholder="Your Full Name" required/>
+                <input type="email" name='email' placeholder="Your Email" required/>
+                <textarea name="messahe" rows="7" placeholder="Your Message" required></textarea>
+                <button type="submit" className='btn btn-primary'>Send Message</button>
+            </form>
         </section>
     )
 }
