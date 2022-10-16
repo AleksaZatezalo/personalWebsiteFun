60fd420e9164da32d0a557ba94936601b7c51a38
Aleksa Zatezalo <aleksa@kali.azatezalo>
2022-10-16 17:01:46 -0400

    Offerings styling completated along with components.

--- react-portfolio-website-1/src/components/servises/Services.jsx
+++ react-portfolio-website-1/src/components/servises/Services.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 import "./services.css"
+import {BiCheck} from "react-icons/bi"
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+        <article className="service">
+          <div className='service__head'>
+            <h3>Cyber Security</h3>
+          </div>
+
+            <ul className='service__list'>
+              <li>
+                <BiCheck/>
+                <p> Service 1</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 2</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 3</p>
+              </li>
+            </ul>
+        </article>
+
+        <article className="service">
+          <div className='service__head'>
+            <h3>Cloud Integration</h3>
+          </div>
+
+            <ul className='service__list'>
+              <li>
+                <BiCheck/>
+                <p> Service 1</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 2</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 3</p>
+              </li>
+            </ul>
+        </article>
+
+
+        <article className="service">
+          <div className='service__head'>
+             <h3>Software Development</h3>
+          </div>
+
+
+            <ul className='service__list'>
+              <li>
+                <BiCheck/>
+                <p> Service 1</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 2</p>
+              </li>
+              <li>
+                <BiCheck/>
+                <p> Service 3</p>
+              </li>
+            </ul>
+        </article>
+
+      </div>
+    </section>
   )
 }
 
